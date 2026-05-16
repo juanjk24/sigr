453175d775f20e9f6f3bdf7bdcff8815999c4c52
juanjk24 <juanjkuellar24@gmail.com>
2026-05-16 18:29:12 -0500

    feat: add reservations pages

---
+++ frontend/src/pages/reservations/ReservationsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReservationsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Reservations</h1>
+    </div>
+  );
+};
+
+export default ReservationsPage;
